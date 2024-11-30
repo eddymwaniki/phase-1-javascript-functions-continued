3811bc6f883ffb96d6327dfcbd9982e1021c360f
eddy <eddy.mwaniki.27@gmail.com>
2024-11-30 16:47:04 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function (visualFlair = '*')
+{ return function (message = 'special')
+   {return `You are ${visualFlair}${message}${visualFlair}!`}
+}
